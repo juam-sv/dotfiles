553e9e2c046aa05ecb4171033c20b6005abd60d3
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-03-05 11:30:58 -0300

    lvim files

--- symb_links.sh
+++ symb_links.sh
@@ -3,10 +3,11 @@
 ### Setup dos dotfiles
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/alacritty.yml ~/.config/alacritty/
-
+ln -s ~/.dotfiles/lvim/config.lua ~/.config/lvim/config.lua
 ln -s ~/.dotfiles/fish ~/.config/fish
 
 
-
+# install
+pacman -S alacritty fish 
 
 
