2316c3624b5ab6050ab3dffd58d9724ff9d20afe
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-03-07 21:13:37 -0300

    add remmina

--- symb_links.sh
+++ symb_links.sh
@@ -6,6 +6,7 @@ rm ~/.config/lvim/config.lua
 rm ~/.config/Abricotine/app/config.json
 rm -rf ~/.config/alacritty #&& mkdir ~/.config/alacritty
 rm -rf ~/.config/fish
+rm -rf ~/.config/remmina
 
 ### Setup dos dotfiles
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -15,5 +16,6 @@ ln -s ~/.dotfiles/config/abricotine/config.json ~/.config/Abricotine/app/
 #folders
 ln -s ~/.dotfiles/config/fish ~/.config
 ln -s ~/.dotfiles/config/alacritty ~/.config/
+ln -s ~/.dotfiles/config/remmina ~/.config/
 
 
