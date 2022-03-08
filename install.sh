39173f7645cf7942cf67b476c21ffccd76e4f530
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-03-07 21:07:31 -0300

    Mudança no install

--- install.sh
+++ install.sh
@@ -7,20 +7,20 @@ mv /tmp/abricotine/dracula ~/.config/Abricotine/app/themes
 # install arch packages
 sudo pacman -S alacritty \
             fish \
-            aur/abricotine btop visual-studio-code-bin --noconfirm
+            aur/abricotine btop notion --noconfirm
 
 #internet
-    firefox google-chrome-stable qbitorrent freedownloadmanager
+    firefox google-chrome-stable qbitorrent freedownloadmanager spotify 
 #social
     discord
     thunderbird
     whatsapp telegram 
 
 #virtualization
-qemu kvm libvirt vagrant 
+qemu kvm libvirt vagrant virt-manager 
 
 #dev
-
+visual-studio-code-bin 
 
 
 
