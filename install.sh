2b50ee161c2985bb7e34a6247a581b05672f0cb4
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-04-21 17:48:38 -0300

    update on packages, orders and reorganization

--- install.sh
+++ install.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-#install yay
-sudo pacman -S yay --noconfirm
-
 # install arch packages
-yay pacman -S alacritty community/fish aur/abricotine btop glances git \
-              yarn npm rust notion-app-enhanced unzip neovim ulauncher \
-              veracrypt tree --noconfirm 
+yay pacman -S --needed yay community/alacritty community/fish btop glances git \
+        yarn npm rust unzip neovim ulauncher \
+        veracrypt tree firefox qbittorrent discord thunderbird \
+        telegram-desktop community/docker community/docker-compose \
+        community/mcfly bat exa community/intel-gpu-tools \ 
+        qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode \
+        ebtables virt-install virt-manager bridge-utils remmina vagrant qemu-arch-extra \
+        virt-install community/ttf-fira-code extra/vlc --noconfirm 
 
-#internet
-yay -S firefox google-chrome qbittorrent freedownloadmanager aur/spotify --noconfirm
-
-#social
-yay -S discord thunderbird whatsapp-nativefier telegram-desktop --noconfirm
-
-#virtualization
-yay -S --needed --noconfirm qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode \
-  ebtables virt-install virt-manager bridge-utils remmina vagrant qemu-arch-extra \
-  virt-install
-  
-#dev
-#yay -S visual-studio-code-bin --noconfirm
-
-#aur packages
-#yay -S  nerd-fonts-fira-code haruna 
+# from AUR and Chaotic
+yay -S aur/spotify aur/abricotine aur/notion-app-enhanced chaotic-aur/google-chrome \
+      chaotic-aur/freedownloadmanager aur/whatsapp-nativefier chaotic-aur/visual-studio-code-bin 
+      chaotic-aur/nerd-fonts-fira-code chaotic-aur/haruna chaotic-aur/authy
 
 #change user shell
 chsh -s /usr/bin/fish $USER
+
+#start and enable services
+systemctl start libvirtd.service
 
 #lunarvim install
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh -o /tmp/install-lvim.sh
@@ -39,7 +32,5 @@ mv /tmp/abricotine/dracula ~/.config/Abricotine/app/themes
 #oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-
 # add user to groups
-sudo addgroup "$(whoami)" libvirt kvm
-community/intel-gpu-tools
+sudo usermod -a -G libvirt,kvm "$(whoami)"
