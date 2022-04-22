d1298e69ccdc3fc8c89b4a1e49fef26c5a1c100e
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-04-22 08:18:55 -0300

    update for fish

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+#add chaotic aur
+bash ./utils/chaotic-aur.sh
+pacman -Syu
+
 # install arch packages
-yay pacman -S --needed yay community/alacritty community/fish btop glances git \
-        yarn npm rust unzip neovim ulauncher \
-        veracrypt tree firefox qbittorrent discord thunderbird \
-        telegram-desktop community/docker community/docker-compose \
-        community/mcfly bat exa community/intel-gpu-tools \ 
-        qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode \
-        ebtables virt-install virt-manager bridge-utils remmina vagrant qemu-arch-extra \
-        virt-install community/ttf-fira-code extra/vlc --noconfirm 
+yay pacman -S yay community/alacritty community/fish btop glances  \
+      yarn npm rust unzip neovim ulauncher \
+      veracrypt tree firefox qbittorrent discord thunderbird \
+      telegram-desktop community/docker community/docker-compose \
+      community/mcfly bat exa community/intel-gpu-tools \ 
+      qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode \
+      ebtables virt-install virt-manager bridge-utils remmina vagrant qemu-arch-extra \
+      virt-install community/ttf-fira-code extra/vlc git pip npm node cargo --noconfirm 
 
 # from AUR and Chaotic
 yay -S aur/spotify aur/abricotine aur/notion-app-enhanced chaotic-aur/google-chrome \
       chaotic-aur/freedownloadmanager aur/whatsapp-nativefier chaotic-aur/visual-studio-code-bin 
       chaotic-aur/nerd-fonts-fira-code chaotic-aur/haruna chaotic-aur/authy
-
-#change user shell
-chsh -s /usr/bin/fish $USER
-
-#start and enable services
-systemctl start libvirtd.service
 
 #lunarvim install
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh -o /tmp/install-lvim.sh
@@ -31,6 +29,12 @@ mv /tmp/abricotine/dracula ~/.config/Abricotine/app/themes
 
 #oh my fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+#start and enable services
+systemctl start libvirtd.service && systemctl enable libvirtd.service
+
+#change user shell
+chsh -s /usr/bin/fish $USER
 
 # add user to groups
 sudo usermod -a -G libvirt,kvm "$(whoami)"
--- symb_links.sh
+++ symb_links.sh
@@ -4,25 +4,33 @@
 rm -rf ~/.gitconfig
 rm -rf ~/.config/lvim/config.lua
 rm -rf ~/.config/Abricotine/app/config.json
-rm -rf ~/.config/vlc/vlcrc
-rm -rf ~/.config/alacritty #&& mkdir ~/.config/alacritty
-rm -rf ~/.config/fish
-rm -rf ~/.config/remmina
-rm -rf ~/.config/omf
-rm -rf ~/.config/haruna && mkdir ~/.config/haruna
+# rm -rf ~/.config/remmina
 
-### Setup dos dotfiles
+# deleting folders
+rm -rf ~/.config/fish && mkdir -p ~/.config/fish
+rm -rf ~/.config/omf && mkdir -p ~/.config/omf
+rm -rf ~/.config/haruna && mkdir ~/.config/haruna
+rm -rf ~/.config/vlc  && mkdir -p ~/.config/vlc
+rm -rf ~/.config/alacritty && mkdir -p ~/.config/alacritty
+rm -rf ~/.config/btop 
+
+#symb link creation for files
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/config/lvim/config.lua ~/.config/lvim/config.lua
 ln -s ~/.dotfiles/config/abricotine/config.json ~/.config/Abricotine/app/config.json
 ln -s ~/.dotfiles/config/haruna/haruna.conf ~/.config/haruna/haruna.conf
 ln -s ~/.dotfiles/config/vlc/vlcrc ~/.config/vlc/vlcrc
 
-#folders
+#symb link creation for folders
 ln -s ~/.dotfiles/config/fish ~/.config
 ln -s ~/.dotfiles/config/alacritty ~/.config/
 ln -s ~/.dotfiles/config/remmina ~/.config/
 ln -s ~/.dotfiles/config/omf ~/.config/
+ln -s ~/.dotfiles/config/btop ~/.config/
+
+#i3 setup
+rm -rf ~/.i3 
+ln -s ~/.dotfiles/config/i3 ~/.i3
 
 #[KDE FILES SECTION]
 #remove files
