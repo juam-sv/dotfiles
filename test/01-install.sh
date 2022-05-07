0890c650ed4f2f76df4ab5fb34b6ace57270fa8c
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-05-07 11:50:59 -0300

    nitrogen package

--- test/01-install.sh
+++ test/01-install.sh
@@ -3,7 +3,7 @@
 # install arch packages
 sudo pacman -S yay community/alacritty community/fish git \
       yarn npm rust unzip neovim ulauncher tree firefox \
-      community/mcfly bat exa community/ttf-fira-code
+      community/mcfly bat exa community/ttf-fira-code nitrogen
 
 # from AUR and Chaotic
 yay -S chaotic-aur/nerd-fonts-fira-code chaotic-aur/nerd-fonts-fantasque-sans-mono
