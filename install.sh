7d76864c7ac4356b7b3d9686e92e31a35bbb971f
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-05-12 19:16:35 -0300

    big update

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ sudo pacman -S yay community/alacritty community/fish btop glances  \
       community/mcfly bat exa community/intel-gpu-tools \ 
       qemu dhclient openbsd-netcat virt-viewer libvirt dnsmasq dmidecode \
       ebtables virt-install virt-manager bridge-utils remmina vagrant qemu-arch-extra \
-      virt-install community/ttf-fira-code extra/vlc git pip npm node cargo \
-      nitrogen polybar bat exa community/mcfly --noconfirm 
+      virt-install community/ttf-fira-code extra/vlc git pip npm cargo \
+      nitrogen polybar bat exa community/mcfly gparted lightdm-settings --noconfirm 
 
 # from AUR and Chaotic
 yay -S aur/spotify aur/abricotine aur/notion-app-enhanced chaotic-aur/google-chrome \
-      chaotic-aur/freedownloadmanager aur/whatsapp-nativefier chaotic-aur/visual-studio-code-bin 
-      chaotic-aur/nerd-fonts-fira-code chaotic-aur/haruna chaotic-aur/authy
+      chaotic-aur/freedownloadmanager aur/whatsapp-nativefier chaotic-aur/visual-studio-code-bin \
+      chaotic-aur/nerd-fonts-fira-code chaotic-aur/haruna chaotic-aur/authy \
       chaotic-aur/nerd-fonts-fantasque-sans-mono
---
+++ screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal
