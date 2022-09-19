64890075ba4ec02c503b55938afe89b590e52548
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-09-18 21:00:59 -0300

    add of asdf and lvim

---
+++ config/fish/completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/juamsv/.asdf/completions/asdf.fish
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -204,3 +204,4 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 #end
 #starship init fish | source
 fish_add_path /home/juamsv/.spicetify
+source /opt/asdf-vm/asdf.fish
