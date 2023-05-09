cdc56ee878dcc2c413605b46f34d2c7bb0153168
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2023-05-09 10:03:08 -0300

    update workjaro

--- test/04-symb_links.sh
+++ test/04-symb_links.sh
@@ -2,7 +2,7 @@
 
 #deleting files
 rm -rf ~/.gitconfig
-rm -rf ~/.config/Abricotine/app/config.json
+#rm -rf ~/.config/Abricotine/app/config.json
 rm -rf ~/.config/mimeapps.list
 rm -rf ~/.redshiftgrc
 # rm -rf ~/.notion-enhancer
@@ -27,7 +27,7 @@ rm -rf ~/.config/kitty
 
 #symb link creation for files
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/config/abricotine/config.json ~/.config/Abricotine/app/config.json
+#ln -s ~/.dotfiles/config/abricotine/config.json ~/.config/Abricotine/app/config.json
 ln -s ~/.dotfiles/config/haruna/haruna.conf ~/.config/haruna/haruna.conf
 ln -s ~/.dotfiles/config/vlc/vlcrc ~/.config/vlc/vlcrc
 ln -s ~/.dotfiles/config/mimeapps.list ~/.config/mimeapps.list
